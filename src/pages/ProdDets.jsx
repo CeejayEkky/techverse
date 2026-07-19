2c27f7f3edd7a99abb84b4caf96b2ee19f41606e
CeejayEkky <ekwomdivinedestiny300@gmail.com>
2026-07-19 12:45:03 +0100

    Lasttt

--- src/pages/ProdDets.jsx
+++ src/pages/ProdDets.jsx
@@ -1,5 +1,5 @@
 import { useParams } from "react-router-dom";
-import products from "../data/products";
+import products from "/data/products";
 import { useContext } from "react";
 import { CartContext } from "../context/CartContext";
 import { WishlistContext } from "../context/WishlistContext";
